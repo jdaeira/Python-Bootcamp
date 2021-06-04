0ee4433906cf1376d86fa5a546844af024be4274
jdaeira <jdaeira@gmail.com>
2021-06-03 17:37:52 -0700

    add Section2

---
+++ Section2/day-2-1-exercise.py
@@ -0,0 +1,12 @@
+# Don't change the code below
+two_digit_number = input("Type a two digit number: ")
+# Don't change the code above
+
+convert_num1 = int(two_digit_number[0])
+convert_num2 = int(two_digit_number[1])
+
+sum = convert_num1 + convert_num2
+
+print(two_digit_number[0] + " + " + two_digit_number[1] + " = " + str(sum))
+####################################
+#Write your code below this line 👇
---
+++ Section2/day-2-math.py
@@ -0,0 +1,20 @@
+# 3 + 5
+# 7 - 4
+# 3 * 2
+# 6 / 3
+# 2 ** 3
+
+# PEMBASLR
+# Parentheses ()
+# Exponents **
+# Multiplication *
+# Division /
+# Addition *
+# Subtraction -
+
+# ()
+# **
+# * /
+# + -
+
+print(3 * 3 + 3 / 3 - 3)
