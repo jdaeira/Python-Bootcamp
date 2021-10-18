58f542c8718735464750a10aac9f57d83397c648
jdaeira <jdaeira@gmail.com>
2021-10-17 17:32:36 -0700

    Start Section 3

---
+++ Section3/day-3-1-exercise.py
@@ -0,0 +1,9 @@
+# Coding Exercise to Look for Odd or Even numbers
+
+number = int(input("Please enter a number: "))
+result = number % 2
+
+if result == 0:
+    print("That number is Even!")
+else:
+    print("That number is Odd!")
---
+++ Section3/day-3-start.py
@@ -0,0 +1,8 @@
+
+print("Welcome to the Roller Coaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the Roller Coaster!")
+else:
+    print("Sorry, but you are too short to ride the Roller Coaster.")
