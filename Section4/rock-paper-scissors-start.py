c60f250842fd4fba2a3d4a08c4d57ebd71e2ab55
jdaeira <jdaeira@gmail.com>
2022-08-04 17:05:16 -0700

    added comments to final Section 4 project

--- Section4/rock-paper-scissors-start.py
+++ Section4/rock-paper-scissors-start.py
@@ -27,22 +27,28 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 
+## Create a list of the ascii images
 images = [rock, paper, scissors]
+
+## Gets the input from the user
 user = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
 
+## Checks to see if the inputed value is valid or now
 if user > len(images) - 1 or user < 0:
     print("That's an invalid entry. You Lose!")
     exit()
-
+## Print the image the user chose
 print(images[user])
 
+## Computer chooses a random number
 computer = random.randint(0, 2)
 
 print("Computer chose:")
 
+## Print the image the computer chose
 print(images[computer])
 
+## Checks to see who won the Game
 if user == computer:
     print("It's a Draw!")
 elif (user == 0 and computer == 2) or (user == 2 and computer == 1) or (user == 1 and computer == 0):
