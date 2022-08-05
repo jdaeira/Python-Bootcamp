e91571fb9e7652e03780253a5e209063d8f06c73
jdaeira <jdaeira@gmail.com>
2022-08-05 14:59:33 -0700

    Changed Section5 Password Generator Code

--- Section5/password-generator-start.py
+++ Section5/password-generator-start.py
@@ -8,49 +8,23 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_letters= int(input("How many letters would you like in your password? ")) 
+nr_symbols = int(input(f"How many symbols would you like? "))
+nr_numbers = int(input(f"How many numbers would you like? "))
 
-letters_list = []
-for _ in range(0, nr_letters):
-    random_letter = random.randint(0, len(letters) - 1)
-    letters_list.append(letters[random_letter])
+password = []
+for _ in range(1, nr_letters + 1):
+    password.append(random.choice(letters))
 
-symbols_list = []
-for _ in range(0, nr_symbols):
-    random_symbol = random.randint(0, len(symbols) - 1)
-    symbols_list.append(symbols[random_symbol])
+for _ in range(1, nr_symbols + 1):
+    password.append(random.choice(symbols))
 
-number_list = []
-for _ in range(0, nr_numbers):
-    random_number = random.randint(0, len(numbers) - 1)
-    number_list.append(numbers[random_number])
+for _ in range(1, nr_numbers + 1):
+    password.append(random.choice(numbers))
 
-password_list = letters_list + symbols_list + number_list
-print(password_list)
-shuffled = random.sample(password_list, len(password_list))
-print(shuffled)
-password = "".join(shuffled)
+print(password)
+random.shuffle(password)
+print(password)
+shuffled_password = "".join(password)
 
-print(f"Your password is: {password}")
-
-# Another way to do it
-
-password2 = ""
-for char in range(1, nr_letters +1):
-    password2 += random.choice(letters)
-
-for char in range(1, nr_symbols +1):
-    password2 += random.choice(symbols)
-
-for char in range(1, nr_numbers +1):
-    password2 += random.choice(numbers)
-
-print(password2)
-
-password2_list = list(password2)
-print(password2_list)
-random.shuffle(password2_list)
-new_password = "".join(password2_list)
-print(f"Your password is: {new_password}")
+print(f"Your password is: {shuffled_password}")
