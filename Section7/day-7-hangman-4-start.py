2088ec4df256f24f9f9b9bbc998370418084bd8a
jdaeira <jdaeira@gmail.com>
2022-08-10 16:08:52 -0700

    Finished Section 7

--- Section7/day-7-hangman-4-start.py
+++ Section7/day-7-hangman-4-start.py
@@ -90,16 +90,17 @@ while not end_of_game:
     #If lives goes down to 0 then the game should stop and it should print "You lose."
     if guess not in chosen_word:
         lives -= 1
+        if lives == 0:
+          end_of_game = True
+          print("You lose!")
     
-    if lives == 0:
-        print("You lose!")
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
     #Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
-        print("You win.")
+        print("You win!")
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
     print(stages[lives])
