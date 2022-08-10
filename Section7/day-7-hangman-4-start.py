0c7ba3da8d719693447ba42fde1dd08359ad0bb0
jdaeira <jdaeira@gmail.com>
2022-08-10 12:27:54 -0700

    Working on Section 7

--- Section7/day-7-hangman-4-start.py
+++ Section7/day-7-hangman-4-start.py
@@ -90,9 +90,9 @@ while not end_of_game:
     #If lives goes down to 0 then the game should stop and it should print "You lose."
     if guess not in chosen_word:
         lives -= 1
+    
     if lives == 0:
         print("You lose!")
-        exit()
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
