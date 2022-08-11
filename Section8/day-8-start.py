612845e72f733560d21bf6edbfa39791cc28ca3d
jdaeira <jdaeira@gmail.com>
2022-08-11 13:53:24 -0700

    Started Section 8

---
+++ Section8/day-8-start.py
@@ -0,0 +1,15 @@
+#Simple Function
+def greet():
+    print("Hello")
+    print("I am inside a function")
+    print("Functions are fun!")
+
+greet()
+
+#Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}!")
+    print(f"How do you do {name}?")
+
+greet_with_name("John")
