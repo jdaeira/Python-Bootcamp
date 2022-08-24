28100a454942ab4dc3fef483fff0d2a7aa517742
jdaeira <jdaeira@gmail.com>
2022-08-24 16:17:16 -0700

    Working on Section 11 Project

--- Section11/main.py
+++ Section11/main.py
@@ -12,18 +12,39 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 ## The computer is the dealer.
 
 user_cards = []
-def get_cards():
-    for num in range(2):
-        card = random.randint(0, 12)
-        user_cards.append(cards[card])
+dealer_cards = []
+def get_cards(player_type, num_cards):
+    if player_type == "user":
+        for _ in range(num_cards):
+            card = random.randint(0, 12)
+            user_cards.append(cards[card])
+    else:
+        for _ in range(num_cards):
+            card = random.randint(0, 12)
+            dealer_cards.append(cards[card])
+
+def get_score(player_list):
+    score = sum(player_list)
+    return score
+
 
 
 
 print(logo)
-
 play = True
-
 while play == True:
     choice = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
-    get_cards()
-    print(user_cards)
+    
+    user_cards = []
+    dealer_cards = []
+
+    if choice == "y":
+        get_cards("user", 2)
+        get_cards("dealer", 2)
+        print(user_cards)
+        print(dealer_cards)
+        print(dealer_cards[0])
+        print(get_score(user_cards))
+        print(get_score(dealer_cards))
+    else:
+        play = False
