ca119b7f66d86fc145fa0bbca6e3f7eaecdb32d7
jdaeira <jdaeira@gmail.com>
2022-08-24 19:04:34 -0700

    Added Functionality to Blackjack Project

--- Section11/main.py
+++ Section11/main.py
@@ -13,6 +13,8 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 user_cards = []
 dealer_cards = []
+user_score = 0
+dealer_score = 0
 def get_cards(player_type, num_cards):
     if player_type == "user":
         for _ in range(num_cards):
@@ -27,7 +29,52 @@ def get_score(player_list):
     score = sum(player_list)
     return score
 
+def play_game():
+    global user_score
+    global dealer_score
 
+    user_score = get_score(user_cards)
+    dealer_score = get_score(dealer_cards)
+
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f"Computer's first card: {dealer_cards[0]}")
+
+    if dealer_score == 21 and user_score == 21:
+        print(f"Computer's final hand: {dealer_cards}, final score: {dealer_score}")
+        print("Push! No One Wins")
+        exit()
+    elif dealer_score == 21 and user_score != 21:
+        print(f"Computer's final hand: {dealer_cards}, final score: {dealer_score}")
+        print("Dealer Wins!")
+        exit()
+
+    deal_cards = True
+    while deal_cards == True:
+        more_cards = input("Would you like another card? Type 'y' or 'n': ").lower()
+
+        if more_cards == "y":
+            get_cards("user", 1)
+            user_score = get_score(user_cards)
+            print(f"Your cards: {user_cards}, current score: {user_score}")
+            print(dealer_cards[0])
+        else:
+            deal_cards = False
+            
+    print(f"Your final hand: {user_cards}, final score: {user_score}")
+
+    while dealer_score < 17:
+        get_cards("dealer", 1)
+        dealer_score = get_score(dealer_cards)
+
+    print(f"Computer's final hand: {dealer_cards}, final score: {dealer_score}")
+
+    test_function()
+
+def test_function():
+    dealer_score = 25
+    user_score = 23
+    print(dealer_score)
+    print(user_score)
 
 
 print(logo)
@@ -39,12 +86,8 @@ while play == True:
     dealer_cards = []
 
     if choice == "y":
-        get_cards("user", 2)
-        get_cards("dealer", 2)
-        print(user_cards)
-        print(dealer_cards)
-        print(dealer_cards[0])
-        print(get_score(user_cards))
-        print(get_score(dealer_cards))
+       get_cards("user", 2)
+       get_cards("dealer", 2)
+       play_game()
     else:
-        play = False
+       play = False
