b06d6ccca510b185fa82e7f741740515ef7eb091
jdaeira <jdaeira@gmail.com>
2022-09-15 14:07:06 -0700

    Finished Section13

--- Section13/day-13-start.py
+++ Section13/day-13-start.py
@@ -24,15 +24,15 @@
 #   print("You are a Gen Z.")
 
 # Fix the Errors
-age = input("How old are you?")
-if age > 18:
-    print(f"You can drive at age {age}.")
+# age = int(input("How old are you? "))
+# if age >= 18:
+#     print(f"You can drive at age {age}.")
 
-# #Print is Your Friend
+#Print is Your Friend
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# word_per_page = int(input("Number of words per page: "))
 # total_words = pages * word_per_page
 # print(total_words)
 
@@ -41,7 +41,7 @@ if age > 18:
 #   b_list = []
 #   for item in a_list:
 #     new_item = item * 2
-#   b_list.append(new_item)
+#     b_list.append(new_item)
 #   print(b_list)
 
 # mutate([1,2,3,5,8,13])
