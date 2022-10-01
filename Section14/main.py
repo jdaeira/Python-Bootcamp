4bbda22c050337fd2e629dfc1fb5fd2b8bfe065a
jdaeira <jdaeira@gmail.com>
2022-09-30 17:01:20 -0700

    Finished Section14 Project

--- Section14/main.py
+++ Section14/main.py
@@ -3,45 +3,42 @@ from game_data import data
 import random
 
 numberA = random.randint(0, len(data) - 1)
-numberB = random.randint(0, len(data) - 1)
-print(numberA)
-print(numberB)
-
-while numberB == numberA:
-    numberB = random.randint(0, len(data) - 1)
-
-print(numberA)
-print(numberB)
-
-print(data[numberA]["follower_count"])
-print(data[numberB]["follower_count"])
-print(len(data))
-
-print(logo)
-
-print(f"Compare A: {data[numberA]['name']}, a {data[numberA]['description']}, from {data[numberA]['country']}")
-
-print(vs)
-
-print(f"Compare B: {data[numberB]['name']}, a {data[numberB]['description']}, from {data[numberB]['country']}")
-
-more = input("Who has more followers? Type 'A' or 'B': ").upper()
-print(more)
 
 score = 0
+win = True
 
-if more == "A":
-    if data[numberA]["follower_count"] > data[numberB]["follower_count"]:
-        score += 1
-        print("You are correct!")
-    else:
-        print("You lost")
-elif more == "B":
-    if data[numberB]["follower_count"] > data[numberA]["follower_count"]:
-        score += 1
-        print("You are correct!")
-    else:
-        print("You lost")
+while win == True:
 
-print(score)
- 
+    numberB = random.randint(0, len(data) - 1)
+    while numberB == numberA:
+        numberB = random.randint(0, len(data) - 1)
+
+    print(logo)
+    if score > 0:
+        print(f"You're right! Current score: {score}")
+
+    print(f"Compare A: {data[numberA]['name']}, a {data[numberA]['description']}, from {data[numberA]['country']}")
+
+    print(vs)
+
+    print(f"Compare B: {data[numberB]['name']}, a {data[numberB]['description']}, from {data[numberB]['country']}")
+
+    more = input("Who has more followers? Type 'A' or 'B': ").upper()
+    
+    if more == "A":
+        if data[numberA]["follower_count"] > data[numberB]["follower_count"]:
+            score += 1
+        else:
+            print(logo)
+            print(f"Sorry, that's wrong. Final score: {score}")
+            win = False
+    elif more == "B":
+        if data[numberB]["follower_count"] > data[numberA]["follower_count"]:
+            score += 1
+            numberA = numberB
+        else:
+            print(logo)
+            print(f"Sorry, that's wrong. Final score: {score}")
+            win = False
+
+print()
